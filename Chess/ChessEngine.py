8438e9cf734cbeff206017a3987f6ebbfdb456f8
aditya <adityajayanti75@gmail.com>
2021-10-03 18:38:34 -0400

    added moving functionality to pieces

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -38,3 +38,34 @@ class GameState:
                     self.board[r, c] = '--'
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = '--'
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+
+class Move:
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                     "5": 3, "6": 2, "7": 1, "8": 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                     "e": 4, "f": 5, "g": 6, "h": 7}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, start_sq, end_sq, board):
+        self.startRow = start_sq[0]
+        self.startCol = start_sq[1]
+        self.endRow = end_sq[0]
+        self.endCol = end_sq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_files[r]
+
+
