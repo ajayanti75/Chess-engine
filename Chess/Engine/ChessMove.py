e5e7bc696bbf80b14d1fd4c81f75ed24407f7abb
aditya <adityajayanti75@gmail.com>
2021-11-15 13:52:13 -0500

    Refactoring

---
+++ Chess/Engine/ChessMove.py
@@ -0,0 +1,36 @@
+# A move class to help with executing moves
+class Move:
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                     "5": 3, "6": 2, "7": 1, "8": 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                     "e": 4, "f": 5, "g": 6, "h": 7}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, start_sq, end_sq, board, is_enpassant_move=False, is_castle_move=False):
+        self.startRow = start_sq[0]
+        self.startCol = start_sq[1]
+        self.endRow = end_sq[0]
+        self.endCol = end_sq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        # pawn promotion
+        self.isPawnPromotion = (self.pieceMoved == 'wp' and self.endRow == 0) or (
+                    self.pieceMoved == 'bp' and self.endRow == 7)
+        # TODO in main, add if pawn promotion, take promotion choice and store in a field here promotion choice, using optional parameters
+        self.isEnpassantMove = is_enpassant_move
+        if self.isEnpassantMove:
+            self.pieceCaptured = 'wp' if self.pieceMoved == 'bp' else 'bp'
+        self.is_castle_move = is_castle_move
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
